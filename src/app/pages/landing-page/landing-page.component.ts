e1b7e5f18c9944a2516d3aca82ebbc933ef5393e
Filip Franik <pacyfist@gmail.com>
2025-03-30 20:50:17 +0200

    font awesome

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faAsterisk,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  /* icons */
+  faAsterisk = faAsterisk;
+  faAngleLeft = faAngleLeft;
+  faAngleRight = faAngleRight;
+}
