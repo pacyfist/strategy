e620b87125ebcf997d613e1a798aaf5bebde3747
Filip Franik <pacyfist@gmail.com>
2025-04-19 14:27:09 +0200

    books and blog now work the same

--- src/app/pages/books-page/books-page.component.ts
+++ src/app/pages/books-page/books-page.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { BlogService } from '../../services/blog.service';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-books-page',
-  imports: [],
+  imports: [RouterLink, DatePipe, FontAwesomeModule],
   templateUrl: './books-page.component.html',
-  styleUrl: './books-page.component.css'
+  styleUrl: './books-page.component.css',
 })
 export class BooksPageComponent {
+  readonly blogService = inject(BlogService);
+  readonly posts = computed(() => this.blogService.posts());
 
+  readonly faAnglesRight = faAnglesRight;
 }
