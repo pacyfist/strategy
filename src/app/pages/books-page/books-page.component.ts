7fbb57f4833d270fe2b2d59ace1bd25f86b60651
Filip Franik <pacyfist@gmail.com>
2025-05-26 23:21:11 +0200

    reactive pages

--- src/app/pages/books-page/books-page.component.ts
+++ src/app/pages/books-page/books-page.component.ts
@@ -1,17 +1,13 @@
-import { DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { PaginatorComponent } from '../../components/paginator/paginator.component';
-import { BooksService } from '../../services/books.service';
 import { SearchComponent } from '../../components/search/search.component';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-books-page',
   imports: [
-    RouterLink,
-    DatePipe,
     FontAwesomeModule,
     PaginatorComponent,
     SearchComponent,
