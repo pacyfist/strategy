465c920905dbd48fd38022f8cf1a358c42e14b36
Filip Franik <pacyfist@gmail.com>
2025-05-29 23:32:05 +0200

    improved book and article

--- src/app/pages/book-page/book-page.component.ts
+++ src/app/pages/book-page/book-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-book-page',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './book-page.component.html',
   styleUrl: './book-page.component.css',
 })
 export class BookPageComponent {
+  readonly faDownload = faDownload
   readonly loading = signal<boolean>(true);
 }
