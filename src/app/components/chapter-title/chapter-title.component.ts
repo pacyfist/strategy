1053a5f183099639c9062fa281c87cb8630ca308
Filip Franik <pacyfist@gmail.com>
2025-06-11 00:18:57 +0200

    refactor: simplify chapter title component and implement visibility detection

--- src/app/components/chapter-title/chapter-title.component.ts
+++ src/app/components/chapter-title/chapter-title.component.ts
@@ -1,4 +1,10 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  input,
+  AfterViewInit,
+  OnDestroy,
+  signal,
+} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,9 +14,31 @@ import { faLink } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './chapter-title.component.html',
   styleUrl: './chapter-title.component.css',
 })
-export class ChapterTitleComponent {
+export class ChapterTitleComponent implements AfterViewInit, OnDestroy {
   readonly faLink = faLink;
 
   readonly title = input.required<string>();
   readonly anchor = input.required<string>();
+
+  readonly isVisible = signal<boolean>(false);
+
+  observer: IntersectionObserver | undefined;
+
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      this.observer = new window.IntersectionObserver(([entry]) => {
+        this.isVisible.set(entry.isIntersecting);
+      });
+
+      const target = document.querySelector('#' + this.anchor());
+
+      if (target) {
+        this.observer?.observe(target);
+      }
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
 }
