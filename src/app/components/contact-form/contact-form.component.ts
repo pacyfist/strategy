4c989d24f929188c84ee274c0743b6351ff23431
Filip Franik <pacyfist@gmail.com>
2025-06-15 23:58:02 +0200

    feat: add InViewClassDirective to contact form component for enhanced visibility

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAsterisk } from '@fortawesome/free-solid-svg-icons';
+import { InViewClassDirective } from '../../directives/in-view-class.directive';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, InViewClassDirective],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
