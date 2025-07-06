963bd58a306092d9003a6c4e4921f482b44f4a00
Filip Franik <pacyfist@gmail.com>
2025-07-06 18:52:42 +0200

    feat: integrate Firebase with environment configuration and store api key in environment dict

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: environment.firebase_apiKey,
+        authDomain: environment.firebase_authDomain,
+        projectId: environment.firebase_projectId,
+        storageBucket: environment.firebase_storageBucket,
+        messagingSenderId: environment.firebase_messagingSenderId,
+        appId: environment.firebase_appId,
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
   ],
 };
