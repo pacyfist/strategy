b6070a5935da6b11b9fec374ab7498d05dfbd688
Filip Franik <pacyfist@gmail.com>
2025-07-12 15:49:31 +0200

    feat: refactor blog article and page components to dynamically render sections from Firestore

--- src/app/pages/blog-article/blog-article.component.ts
+++ src/app/pages/blog-article/blog-article.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { PageTitleComponent } from '../../components/page-title/page-title.component';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PageTeaserComponent } from '../../components/page-teaser/page-teaser.component';
+import { PageTitleComponent } from '../../components/page-title/page-title.component';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog-article',
-  imports: [PageTitleComponent, PageTeaserComponent],
+  imports: [PageTitleComponent, PageTeaserComponent, AsyncPipe],
   templateUrl: './blog-article.component.html',
   styleUrl: './blog-article.component.css',
 })
-export class BlogArticleComponent {}
+export class BlogArticleComponent {
+  readonly route = inject(ActivatedRoute);
+  readonly blogService = inject(BlogService);
+
+  readonly id = this.route.snapshot.params['id'];
+  readonly sections = this.blogService.getSections(this.id);
+}
