087c40028b7485130952a11a81deb7a7eda480fa
Filip Franik <pacyfist@gmail.com>
2025-07-12 20:48:59 +0200

    feat: add environment configuration for Firebase in development

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase_apiKey: '',
+  firebase_authDomain: '',
+  firebase_projectId: '',
+  firebase_storageBucket: '',
+  firebase_messagingSenderId: '',
+  firebase_appId: '',
+};
+
