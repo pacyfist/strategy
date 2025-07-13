3b699a7c37f9f415ee2bd08d13f3d49f1365c9c2
Filip Franik <pacyfist@gmail.com>
2025-07-13 21:28:09 +0200

    feat: configured hybrid rendering for login page

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,6 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  { path: 'login', renderMode: RenderMode.Client },
+  { path: '**', renderMode: RenderMode.Prerender },
+];
