b3b38ed82f6c381c01f375fee2a9518fa8cd9491
Filip Franik <pacyfist@gmail.com>
2025-07-13 22:28:27 +0200

    feat: refactor login page to use reactive forms and improve user input handling

--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -1,28 +1,42 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PageTitleComponent } from '../../../components/page-title/page-title.component';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [PageTitleComponent, FormsModule],
+  imports: [PageTitleComponent, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {
   readonly auth = inject(AuthService);
 
-  readonly email = signal<string>('');
-  readonly password = signal<string>('');
+  readonly form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
   readonly failed = signal<boolean>(false);
 
-  async loginClick() {
+  async signIn() {
     this.failed.set(false);
 
     try {
-      await this.auth.signIn(this.email(), this.password());
+      await this.auth.signIn(
+        this.form.controls.email.value ?? '',
+        this.form.controls.password.value ?? '',
+      );
     } catch (ex) {
       this.failed.set(true);
     }
+  }
+
+  async signOut() {
+    await this.auth.signOut();
   }
 }
