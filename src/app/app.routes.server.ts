5980d5d71f3911251ec58445173c70448d9bff20
Filip Franik <pacyfist@gmail.com>
2025-07-14 21:13:36 +0200

    refactor: remove hybrid rendering

--- src/app/app.routes.server.ts
+++
@@ -1,6 +0,0 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  { path: 'login', renderMode: RenderMode.Client },
-  { path: '**', renderMode: RenderMode.Prerender },
-];
