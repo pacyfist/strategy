90b6c314c670aa3b5efd12ed5a642bc3197792cb
Filip Franik <pacyfist@gmail.com>
2025-07-17 20:14:35 +0200

    refactor: moved blog-page to blog folder

--- src/app/pages/blog/blog-page/blog-page.component.ts
+++ src/app/pages/blog/blog-page/blog-page.component.ts
@@ -2,9 +2,9 @@ import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { mergeMap } from 'rxjs';
-import { PageLeadComponent } from '../../components/page-lead/page-lead.component';
-import { PageTitleComponent } from '../../components/page-title/page-title.component';
-import { BlogService } from '../../services/blog.service';
+import { PageLeadComponent } from '../../../components/page-lead/page-lead.component';
+import { PageTitleComponent } from '../../../components/page-title/page-title.component';
+import { BlogService } from '../../../services/blog.service';
 
 @Component({
   selector: 'app-blog-article',
