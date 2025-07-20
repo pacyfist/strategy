eade8fef7dea15de453ed18898c30338567d823e
Filip Franik <pacyfist@gmail.com>
2025-07-20 11:44:02 +0200

    feat: integrate Quill editor for section HTML rendering in blog page

--- src/app/pages/blog/blog-page/blog-page.component.ts
+++ src/app/pages/blog/blog-page/blog-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { QuillViewHTMLComponent } from 'ngx-quill';
 import { mergeMap } from 'rxjs';
 import { PageLeadComponent } from '../../../components/page-lead/page-lead.component';
 import { PageTitleComponent } from '../../../components/page-title/page-title.component';
 import { BlogService } from '../../../services/blog.service';
 
 @Component({
-  imports: [PageTitleComponent, PageLeadComponent],
+  imports: [PageTitleComponent, PageLeadComponent, QuillViewHTMLComponent],
   templateUrl: './blog-page.component.html',
 })
 export class BlogPageComponent {
